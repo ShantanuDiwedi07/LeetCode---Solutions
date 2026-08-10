6e0dc3ae8c0a9b236d612de73736263c14f48892
Shantanu Diwedi <shantanudiwedi2007@gmail.com>
2026-08-10 22:57:02 +0530

    Delete Easy/9-palindrome-number directory

--- Easy/9-palindrome-number/palindrome-number.py
+++
@@ -1,8 +0,0 @@
-class Solution ():
-    def isPalindrome(self,n): 
-        num = str(n)
-        num2 = num[::-1]
-        if num==num2 :
-            return True
-        else :
-            return False
